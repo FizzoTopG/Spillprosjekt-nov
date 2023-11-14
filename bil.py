c756f3de680b03c10828149d7074eeb622bbd18e
FizzoTopG <faizan.m05@icloud.com>
2023-11-14 14:44:10 +0100

    mye

--- bil.py
+++ bil.py
@@ -1,3 +1,16 @@
 from figur import Figur
+import pygame
 
 class Bil(Figur):
+    def __init__(self, vindu_bredde: int, vindu_høyde: int) -> None:
+        super().__init__("bilder/pixel-r8.png")
+        self.poeng = 0
+        self.liv = 3
+        self.bilde = pygame.transform.scale_by(self.bilde, 0.2)
+        self.ramme = self.bilde.get_rect()
+        #setter bilen i startposisjon
+        self.ramme.centerx = vindu_bredde / 2
+        self.ramme.bottom = vindu_høyde 
+
+    def flytt(self, dx: int):
+        self.ramme.x += dx
